47ab50028b29ae62fd13d763cea009569805b0ba
Daniel Stahl <danstahl1138@gmail.com>
2020-03-14 17:19:24 -0500

    Fixed a lot

--- src/index.js
+++ src/index.js
@@ -1,102 +1,101 @@
-const ftch=global.fetch||require('node-fetch')
-const resJson=res=>res.json()
-const generic_calculation=({
-    body, version, optionType, includeIV=false,
-    model, sensitivity, headers, url
-})=>ftch(
-    `${url}/${version}/${model}/calculator/${optionType}/${sensitivity}?includeImpliedVolatility=${includeIV}`,
+const ftch = global.fetch || require('node-fetch')
+const resJson = res => res.json()
+const getVersion = version => version === '' ? version : version + '/'
+const generic_calculation = ({
+    body, version, optionType, includeIV = false,
+    model, sensitivity, apiKey, url
+}) => ftch(
+    `${url}/${getVersion(version)}${model}/calculator/${optionType}/${sensitivity}?include_implied_volatility=${includeIV}&access_token=${apiKey}`,
     {
-        method:'POST',
-        body:JSON.stringify(body),
-        headers
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {}
     }).then(resJson)
 
-const generic_density=({
-    body, version, model, 
-    headers, metric, url
-})=>ftch(
-    `${url}/${version}/${model}/${metric}`,
+const generic_density = ({
+    body, version, model,
+    apiKey, metric, url
+}) => ftch(
+    `${url}/${getVersion(version)}${model}/${metric}?access_token=${apiKey}`,
     {
-        method:'POST',
-        body:JSON.stringify(body),
-        headers
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {}
     }).then(resJson)
 
-const generic_constraints=({
-    model, headers, 
+const generic_constraints = ({
+    model, apiKey,
     version, url
-})=>ftch(
-    `${url}/${version}/${model}/parameters/parameter_ranges`,
-    {headers}).then(resJson)
+}) => ftch(
+    `${url}/${getVersion(version)}${model}/parameters/parameter_ranges?access_token=${apiKey}`,
+    { headers: {} }).then(resJson)
 
-const init=({
-    api_key, version, 
-    url='https://api.finside.org/realoptions'
-})=>{
-    const headers={
-        'x-api-Key':api_key
-    }
+const init = ({
+    api_key, version,
+    url = 'https://api2.finside.org/realoptions'
+}) => {
+
     return {
-        cgmy:{
-            options:(body, optionType, sensitivity, includeIV)=>generic_calculation({
+        cgmy: {
+            options: (body, optionType, sensitivity, includeIV) => generic_calculation({
                 body, version, optionType, includeIV,
-                sensitivity, headers, url, model:'cgmy'
+                sensitivity, apiKey: api_key, url, model: 'cgmy'
             }),
-            density:(body)=>generic_density({
-                body, model:'cgmy', version, 
-                headers, url, metric:'density'
+            density: (body) => generic_density({
+                body, model: 'cgmy', version,
+                apiKey: api_key, url, metric: 'density'
             }),
-            riskmetric:(body)=>generic_density({
-                body, model:'cgmy', version, 
-                headers, url, metric:'riskmetric'
+            riskmetric: (body) => generic_density({
+                body, model: 'cgmy', version,
+                apiKey: api_key, url, metric: 'riskmetric'
             }),
-            constraints:()=>generic_constraints({
-                model:'cgmy', version, 
-                headers, url
+            constraints: () => generic_constraints({
+                model: 'cgmy', version,
+                apiKey: api_key, url
             })
         },
-        heston:{
-            options:(body, optionType, sensitivity, includeIV)=>generic_calculation({
+        heston: {
+            options: (body, optionType, sensitivity, includeIV) => generic_calculation({
                 body, version, optionType, url, includeIV,
-                sensitivity, headers, model:'heston'
+                sensitivity, apiKey: api_key, model: 'heston'
             }),
-            density:(body)=>generic_density({
-                body, model:'heston', version, 
-                headers, metric:'density', url
+            density: (body) => generic_density({
+                body, model: 'heston', version,
+                apiKey: api_key, metric: 'density', url
             }),
-            riskmetric:(body)=>generic_density({
-                body, model:'heston', version, 
-                headers, metric:'riskmetric', url
+            riskmetric: (body) => generic_density({
+                body, model: 'heston', version,
+                apiKey: api_key, metric: 'riskmetric', url
             }),
-            constraints:()=>generic_constraints({
-                model:'heston', version, 
-                headers, url
+            constraints: () => generic_constraints({
+                model: 'heston', version,
+                apiKey: api_key, url
             })
         },
-        merton:{
-            options:(body, optionType, sensitivity, includeIV)=>generic_calculation({
+        merton: {
+            options: (body, optionType, sensitivity, includeIV) => generic_calculation({
                 body, version, optionType, url, includeIV,
-                sensitivity, headers, model:'merton'
+                sensitivity, apiKey: api_key, model: 'merton'
             }),
-            density:(body)=>generic_density({
-                body, model:'merton', version, 
-                headers, metric:'density', url
+            density: (body) => generic_density({
+                body, model: 'merton', version,
+                apiKey: api_key, metric: 'density', url
             }),
-            riskmetric:(body)=>generic_density({
-                body, model:'merton', version, 
-                headers, metric:'riskmetric', url
+            riskmetric: (body) => generic_density({
+                body, model: 'merton', version,
+                apiKey: api_key, metric: 'riskmetric', url
             }),
-            constraints:()=>generic_constraints({
-                model:'merton', version, 
-                headers, url
+            constraints: () => generic_constraints({
+                model: 'merton', version,
+                apiKey: api_key, url
             })
         },
-        market:{
-            constraints:()=>generic_constraints({
-                model:'market', version, 
-                headers, url
+        market: {
+            constraints: () => generic_constraints({
+                model: 'market', version,
+                apiKey: api_key, url
             })
         }
     }
 }
-module.exports=init
+module.exports = init
