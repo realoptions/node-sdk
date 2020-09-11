05d46e2a2865b7b3db3cdf8b5d1bb2fcab5f6f0b
Daniel Stahl <danstahl1138@gmail.com>
2020-09-11 07:46:27 -0500

    fixed fetch yaml

--- scripts/fetch_yaml.sh
+++ scripts/fetch_yaml.sh
@@ -1,1 +1,1 @@
-wget -O ./mocks/openapi.yaml https://raw.githubusercontent.com/realoptions/option_price_faas/master/docs/openapi_v2.yml
+wget -O ./mocks/openapi.yaml https://raw.githubusercontent.com/realoptions/option_price_faas/master/docs/openapi_gcp.yml
