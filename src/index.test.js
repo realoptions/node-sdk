a1568f5110f2d86ea119962211054f6e2a9eb5fd
Daniel Stahl <danstahl1138@gmail.com>
2022-01-15 11:12:52 -0600

    updated and simplifeid

--- src/index.test.js
+++ src/index.test.js
@@ -14,6 +14,21 @@ const hestonParameters = {
         rho: -.5
     },
     strikes: [50],
+}
+const hestonDensityParameters = {
+    num_u: 8,
+    rate: 0.04,
+    maturity: 1,
+    cf_parameters: {
+        sigma: 0.3,
+        speed: 0.4,
+        v0: 0.3,
+        eta_v: 0.2,
+        rho: -.5
+    },
+}
+const hestonMetricParameters = {
+    ...hestonDensityParameters,
     quantile: 0.01
 }
 const mertonParameters = {
@@ -32,6 +47,24 @@ const mertonParameters = {
         lambda: 0.2
     },
     strikes: [50],
+}
+const mertonDensityParameters = {
+    num_u: 8,
+    rate: 0.04,
+    maturity: 1,
+    cf_parameters: {
+        sigma: 0.3,
+        speed: 0.4,
+        v0: 0.9,
+        eta_v: 0.2,
+        rho: -.5,
+        sig_l: 0.2,
+        mu_l: 0.3,
+        lambda: 0.2
+    },
+}
+const mertonMetricParameters = {
+    ...mertonDensityParameters,
     quantile: 0.01
 }
 const cgmyParameters = {
@@ -51,6 +84,25 @@ const cgmyParameters = {
         y: 0.5
     },
     strikes: [50],
+}
+const cgmyDensityParameters = {
+    num_u: 8,
+    rate: 0.04,
+    maturity: 1,
+    cf_parameters: {
+        sigma: 0.3,
+        speed: 0.4,
+        v0: 0.9,
+        eta_v: 0.2,
+        rho: -.5,
+        c: 1,
+        g: 5,
+        m: 5,
+        y: 0.5
+    },
+}
+const cgmyMetricParameters = {
+    ...cgmyDensityParameters,
     quantile: 0.01
 }
 
@@ -60,13 +112,14 @@ it('correctly calls cgmy constraints', () => {
     })
 })
 it('correctly calls cgmy density', () => {
-    return ro.cgmy.density(cgmyParameters).then(results => {
+    return ro.cgmy.density(cgmyDensityParameters).then(results => {
+        console.log(results)
         return expect(Array.isArray(results)).toEqual(true)
     })
 })
 it('correctly calls cgmy riskmetrics', () => {
     let results
-    return ro.cgmy.riskmetric(cgmyParameters).then(results_ => {
+    return ro.cgmy.riskmetric(cgmyMetricParameters).then(results_ => {
         results = results_
         return expect(results.value_at_risk).toBeDefined()
     }).then(() => {
@@ -74,7 +127,7 @@ it('correctly calls cgmy riskmetrics', () => {
     })
 })
 it('correctly calls cgmy options', () => {
-    return ro.cgmy.options(mertonParameters, 'call', 'price').then(results => {
+    return ro.cgmy.options(cgmyParameters, 'call', 'price').then(results => {
         return expect(Array.isArray(results)).toEqual(true)
     })
 })
@@ -84,13 +137,13 @@ it('correctly calls merton constraints', () => {
     })
 })
 it('correctly calls merton density', () => {
-    return ro.merton.density(mertonParameters).then(results => {
+    return ro.merton.density(mertonDensityParameters).then(results => {
         return expect(Array.isArray(results)).toEqual(true)
     })
 })
 it('correctly calls merton riskmetrics', () => {
     let results
-    return ro.merton.riskmetric(mertonParameters).then(results_ => {
+    return ro.merton.riskmetric(mertonMetricParameters).then(results_ => {
         results = results_
         return expect(results.value_at_risk).toBeDefined()
     }).then(() => {
@@ -108,20 +161,20 @@ it('correctly calls heston constraints', () => {
     })
 })
 it('correctly calls heston density', () => {
-    return ro.heston.density(hestonParameters).then(results => {
+    return ro.heston.density(hestonDensityParameters).then(results => {
         return expect(Array.isArray(results)).toEqual(true)
     })
 })
 it('correctly calls heston riskmetrics', () => {
     let results
-    return ro.heston.riskmetric(hestonParameters).then(results_ => {
+    return ro.heston.riskmetric(hestonMetricParameters).then(results_ => {
         results = results_
         return expect(results.value_at_risk).toBeDefined()
     }).then(() => {
         return expect(results.expected_shortfall).toBeDefined()
     })
 })
-it('correctly calls merton options', () => {
+it('correctly calls heston options', () => {
     return ro.heston.options(hestonParameters, 'call', 'price').then(results => {
         return expect(Array.isArray(results)).toEqual(true)
     })
